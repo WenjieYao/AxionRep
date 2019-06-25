ade2a229306aa301831c02889b51a88bbd6a6d33
Wenjie Yao <jayyao@mit.edu>
2019-06-25 10:15:39 -0400

    new

--- PIT.py
+++ PIT.py
@@ -1,89 +1,21 @@
-#%The supercell gemoetry
 import math
 import meep as mp
+from meep import mpb
+num_bands = 8
+k_points = [mp.Vector3(),               # Gamma
+            mp.Vector3(y=0.5),          # M
+            mp.Vector3(-1 / 3, 1 / 3),  # K
+            mp.Vector3()]               # Gamma
 
-#na = 1
-#r = 0.0725
-#n = math.sqrt(32)
-def DiamondLattice(n,r,nx,ny,nz,ax,ay,az):
-    #
-    L = math.sqrt(3)/4.0
-    hs = math.sqrt(3)/12.0
-
-    geometry_lattice = mp.Lattice(size=mp.Vector3(nx*ax,ny*ay,nz*az))
-    #v1 = mp.Vector3(1)
-
-    #displace vector to fill the whole supercell
-    v2 = mp.Vector3(0.5*ax,0.5*ay,0)
-    v1 = mp.Vector3(0,ay*1.0/3.0,az*1.0/3.0)
-    v5 = mp.Vector3(ax*0.5,ay*5.0/6.0,az*1.0/3.0)
-    v3 = mp.Vector3(ax*0.5,ay*1.0/6.0,az*2.0/3.0)
-    v4 = mp.Vector3(0,ay*2.0/3.0,az*2.0/3.0)
-
-    m = mp.Medium(epsilon=n*n)
-    #center of the four supercell rod
-    c1 = mp.Vector3(0,-math.sqrt(6)/12,-hs/2)
-    c2 = mp.Vector3(math.sqrt(2)/8,math.sqrt(6)/24,-hs/2)
-    c3 = mp.Vector3(-math.sqrt(2)/8,math.sqrt(6)/24,-hs/2)
-    c4 = mp.Vector3(0,0,L/2)
-    geometry = [mp.Cylinder(center=c1,radius=r, material=m, height=L, axis=c1),
-            mp.Cylinder(center=c2,radius=r, material=m, height=L, axis=c2),
-            mp.Cylinder(center=c3,radius=r, material=m, height=L, axis=c3),
-            mp.Cylinder(center=c4,radius=r, material=m, height=L, axis=c4),
-            mp.Cylinder(center=c1+v1,radius=r, material=m, height=L, axis=c1),
-            mp.Cylinder(center=c2+v1,radius=r, material=m, height=L, axis=c2),
-            mp.Cylinder(center=c3+v1,radius=r, material=m, height=L, axis=c3),
-            mp.Cylinder(center=c4+v1,radius=r, material=m, height=L, axis=c4),
-            mp.Cylinder(center=c1+v2,radius=r, material=m, height=L, axis=c1),
-            mp.Cylinder(center=c2+v2,radius=r, material=m, height=L, axis=c2),
-            mp.Cylinder(center=c3+v2,radius=r, material=m, height=L, axis=c3),
-            mp.Cylinder(center=c4+v2,radius=r, material=m, height=L, axis=c4),
-            mp.Cylinder(center=c1+v3,radius=r, material=m, height=L, axis=c1),
-            mp.Cylinder(center=c2+v3,radius=r, material=m, height=L, axis=c2),
-            mp.Cylinder(center=c3+v3,radius=r, material=m, height=L, axis=c3),
-            mp.Cylinder(center=c4+v3,radius=r, material=m, height=L, axis=c4),
-            mp.Cylinder(center=c1+v4,radius=r, material=m, height=L, axis=c1),
-            mp.Cylinder(center=c2+v4,radius=r, material=m, height=L, axis=c2),
-            mp.Cylinder(center=c3+v4,radius=r, material=m, height=L, axis=c3),
-            mp.Cylinder(center=c4+v4,radius=r, material=m, height=L, axis=c4),
-            mp.Cylinder(center=c1+v5,radius=r, material=m, height=L, axis=c1),
-            mp.Cylinder(center=c2+v5,radius=r, material=m, height=L, axis=c2),
-            mp.Cylinder(center=c3+v5,radius=r, material=m, height=L, axis=c3),
-            mp.Cylinder(center=c4+v5,radius=r, material=m, height=L, axis=c4)]
-
-    geometry = mp.geometric_objects_lattice_duplicates(geometry_lattice, geometry, ax,ay,az)
-    #geometry.append([mp.Cylinder(center=c4,radius=r, material=mp.air, height=L, axis=c4)])
-    return geometry, geometry_lattice
-
-#supercell size
-nx = 7
-ny = 4
-nz = 3
-r = 0.0725 #rod raius
-n = math.sqrt(32) #rod refractive index
-#lattice vectors
-ax = math.sqrt(2)/2.0
-ay = math.sqrt(6)/2.0
-az = math.sqrt(3)
-#get the geometry and cell
-geometry,geometry_lattice=DiamondLattice(n,r,nx,ny,nz,ax,ay,az)
-cell = mp.Vector3(nx*ax,ny*ay,nz*az)
-
-Th = 100
-ITER = 1
-resolution = 15
-dpml = 1
-pml_layers = [mp.PML(dpml)]
-pos = mp.Vector3(0.1)
-for li in range(0,ITER):
-    fcen = (li+1)*0.05+0.6
-    df = fcen/10.0
-    #sources = [mp.Source(src=mp.GaussianSource(fcen, fwidth=df), component=mp.Ez,
-    #                         center=pos)]
-    #symmetries = [mp.Mirror(mp.Y)]
-    sim = mp.Simulation(cell_size=cell,
-                        geometry=geometry,
-                        boundary_layers=pml_layers,
-                        resolution=resolution)
-    h = mp.Harminv(mp.Ez, pos, fcen, df)
-    sim.run(mp.after_sources(h), until_after_sources=Th)
+k_points = mp.interpolate(4, k_points)
+geometry = [mp.Cylinder(center=mp.Vector3(),radius=0.2,height=0.1,axis=mp.Vector3(0,0,1), material=mp.Medium(epsilon=12))]
+geometry_lattice = mp.Lattice(size=mp.Vector3(5, 5, 0.1))
+mp.geometric_objects_lattice_duplicates(geometry_lattice, geometry, 1,1,0.1)
+resolution = 20
+ms = mpb.ModeSolver(num_bands=num_bands,
+                    k_points=k_points,
+                    geometry=geometry,
+                    geometry_lattice=geometry_lattice,
+                    resolution=resolution)
+ms.run_tm()
+ms.output_epsilon()
